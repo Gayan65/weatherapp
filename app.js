b6e8f7af4bac77f9d25388c45729bfde7beb8b68
gayan65728@yahoo.com <egawi001@edu.xamk.fi>
2024-02-01 18:45:21 +0200

    Mongo DB connected successfully

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 import express from "express";
 import "dotenv/config";
 import weather_router from "./routes/weather_data.js";
+import mongoose from "mongoose";
 
 const app = express();
 const port = process.env.PORT;
+const urlDB = process.env.URL;
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(port, () => {
-    console.log(`Server running in port : ${port}`);
+await mongoose.connect(urlDB).then(() => {
+    console.log("DB Connected!");
+    app.listen(port, () => {
+        console.log(`Server running in port : ${port}`);
+    });
 });
