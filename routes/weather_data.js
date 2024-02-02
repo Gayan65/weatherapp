2a23ba4ffa5bc5f948e64fc2b5aae188af3bb9ca
gayan65728@yahoo.com <egawi001@edu.xamk.fi>
2024-02-02 13:31:12 +0200

    save temp data to db successful!

--- routes/weather_data.js
+++ routes/weather_data.js
@@ -9,30 +9,38 @@ weather_router.use(bodyParser.json());
 //API for the weather data create
 weather_router.post("/weather/create", async (req, res) => {
     const mqttMessage = req.body.message;
-    console.log(mqttMessage);
-    console.log("Data from mqtt received successfully to create api");
+    //Try to convert the JSON OBJ to JS OBJ
+    try {
+        var jsObject = JSON.parse(mqttMessage);
+        //console.log("Parsed JavaScript Object:", jsObject);
+        console.log("Data from mqtt received successfully to create api");
+        const { temperature, humidity } = jsObject;
+        const weather = new Weather({
+            temperature: temperature,
+            humidity: humidity,
+            //createDate: dateAndTime,
+        });
+
+        try {
+            await weather.save().then((weatherData) => {
+                return res.status(200).json({
+                    success: true,
+                    message: "Weather data successfully saved to DB!",
+                    weatherData: weatherData,
+                });
+            });
+        } catch (error) {
+            return res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    } catch (error) {
+        console.error("Error parsing JSON:", error.message);
+    }
 
     /*
-    const weather = new Weather({
-        temperature: temperature,
-        humidity: humidity,
-        createDate: dateAndTime,
-    });
 
-    try {
-        await weather.save().then((weatherData) => {
-            return res.status(200).json({
-                success: true,
-                message: "Weather data successfully saved to DB!",
-                weatherData: weatherData
-            })
-        });
-    } catch (error) {
-        return res.status(400).json({
-            success: false,
-            message: error.message
-        })
-    }
     */
 });
 
--- schemas/weather_data.js
+++ schemas/weather_data.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const weatherSchema = new mongoose.Schema({
     temperature: String,
     humidity: String,
-    createDate: Date,
+    //createDate: Date,
 });
 
 export const Weather = mongoose.model("Weather", weatherSchema);
