87ecd31f3aa178bbf61888aa90711731662e282d
gayan65728@yahoo.com <egawi001@edu.xamk.fi>
2024-02-06 10:26:07 +0200

    new code

--- arduino.c
+++ arduino.c
@@ -4,7 +4,7 @@
 
 const char* WIFI_SSID = "MB210-G";
 const char* WIFI_PASSWORD = "studentMAMK";
-const char* MQTT_BROKER = "172.20.49.18";
+const char* MQTT_BROKER = "172.20.49.6";
 const int MQTT_PORT = 1883;
 const char* MQTT_TOPIC = "test";
 
@@ -31,6 +31,94 @@ void loop() {
 
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
+
+  if (!isnan(temperature) && !isnan(humidity)) {
+    publishData(temperature, humidity);
+  }
+
+  delay(5000);  // Adjust as needed
+}
+
+void setupWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void publishData(float temperature, float humidity) {
+  String payload = "{\"temperature\":" + String(temperature) + ",\"humidity\":" + String(humidity) + "}";
+  client.publish(MQTT_TOPIC, payload.c_str());
+}
+
+
+
+
+
+
+// NEW CODE
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>
+
+
+const char* WIFI_SSID = "MB210-G";
+const char* WIFI_PASSWORD = "studentMAMK";
+const char* MQTT_BROKER = "172.20.49.6";
+const int MQTT_PORT = 1883;
+const char* MQTT_TOPIC = "test";
+const int MEASUREMENTS = 10;
+
+const int DHT_PIN = 26;  // Replace with your DHT11 data pin
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+DHT dht(DHT_PIN, DHT11);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  delay(2000);
+  setupWiFi();
+  client.setServer(MQTT_BROKER, MQTT_PORT);
+}
+
+
+void loop() {
+      float temperature = 0;
+   float humidity = 0;
+  if (!client.connected()) {
+    reconnect();
+
+  }
+
+for (int i = 0; i < MEASUREMENTS; i++) {
+  temperature +=dht.readTemperature();
+  humidity += dht.readHumidity();
+   delay(1000);
+}
+  client.loop();
+ 
+  temperature /= MEASUREMENTS;
+  humidity /= MEASUREMENTS;
 
   if (!isnan(temperature) && !isnan(humidity)) {
     publishData(temperature, humidity);
